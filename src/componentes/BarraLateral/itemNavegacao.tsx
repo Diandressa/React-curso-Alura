21f9619f10dc7159c784d1c7764a676a2aacce40
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-05-20 15:45:17 -0300

    condicao props para style component

--- src/componentes/BarraLateral/itemNavegacao.tsx
+++ src/componentes/BarraLateral/itemNavegacao.tsx
@@ -7,11 +7,24 @@ interface Props {
     ativo?:boolean
 }
 
-const ItemListaEstilizado = styled.li``
+interface EstiloProps {
+    $ativo?: boolean
+}
+
+const ItemListaEstilizado = styled.li<EstiloProps>`
+    font-size: 24px;
+    line-height: 29px;
+    margin-bottom: 30px;
+    cursor: pointer;
+    color: ${props => props.$ativo ? '#7b78e5' : '#d9d9d9'};
+    display: flex;
+    align-items: center;
+    gap: 22px;
+`
 
 const ItemNavegacao = ({children, iconeAtivo, iconeInativo, ativo = false}:Props) => {
     return(
-        <ItemListaEstilizado>
+        <ItemListaEstilizado $ativo={ativo}>
             <img src={ativo ? iconeAtivo : iconeInativo} alt="icone menu" />
             {children}
         </ItemListaEstilizado>
