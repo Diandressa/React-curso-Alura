a1f26231bca44bb1c62f73c39c73e06106fec691
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-05-22 13:58:58 -0300

    tags

--- src/componentes/Galeria/Tags/index.tsx
+++ src/componentes/Galeria/Tags/index.tsx
@@ -1,6 +1,14 @@
+import tags from './tags.json'
+
 const Tags = () => {
     return(
-        <p>Tags</p>
+        <>
+            <p>Busque por tags:</p>
+            <div>
+                
+            </div>
+            {tags.map(tag => <button key={tag.id}>{tag.titulo}</button>)}
+        </>
     )
 }
 
