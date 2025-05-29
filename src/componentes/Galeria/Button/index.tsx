5a28e92d25a529d25ffa9dc5064b8c3fa53b72c5
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-05-29 14:26:50 -0300

    card imagens da galeria

---
+++ src/componentes/Galeria/Button/index.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+interface buttonProps{
+    src:string;
+    alt:string;
+}
+
+const Button = styled.button`
+    background: transparent;
+    border: none;
+    padding: 0;
+    margin-inline: 5px;
+
+    img{
+        margin-left: 5px;
+    }
+`
+const ButtonImage = ({src, alt}:buttonProps) => {
+    return(
+        <Button>
+            <img src={src} alt={alt} /> 
+        </Button>
+    )
+}
+
+export default ButtonImage;
--- src/componentes/Galeria/Imagem/index.tsx
+++ src/componentes/Galeria/Imagem/index.tsx
@@ -1,3 +1,6 @@
+import styled from "styled-components";
+import ButtonImage from "../Button";
+
 interface Foto {
   titulo: string;
   fonte: string;
@@ -9,19 +12,64 @@ interface Foto {
 interface ImagemProps {
     foto:Foto;
 } 
+
+const Figure = styled.figure`
+    background-color: #fff;
+    border-radius: 20px;
+    max-width: 100%;
+
+    figcaption{
+        background-color: #001634;
+        color: #fff;
+        border-radius: 0 0 20px 20px;
+        padding: 1rem;
+        display: flex;
+        align-items: end;
+        justify-content: space-between;
+
+        p {
+            margin-bottom: 10px;
+            margin-top: 0px;
+        }
+    }
+
+    h3{
+        margin: 0;
+        color: #fff;
+        font-size: 1rem;
+        font-weight: bold;
+    }
+`  
+
+const Img = styled.img`
+    width: 100%;
+    border-radius: 20px 20px 0 0;
+    display: block;
+`
+
+const Rodape = styled.footer`
+    
+`
+const Div = styled.div`
+    
+`
+
+
 const Imagem = ({foto}:ImagemProps) => {
     return(
-        <figure>
-            <img src={foto.path} alt={foto.titulo} />
+        <Figure>
+            <Img src={foto.path} alt={foto.titulo} />
             <figcaption>
-                <h3>Título</h3>
-                <footer>
-                    <p>fonte</p>
-                    <button>favorito</button>
-                    <button>expandir</button>
-                </footer>
+                <Div>
+                    <h3>{foto.titulo}</h3>
+                    <p>{foto.fonte}</p>
+                </Div>
+                <Rodape>
+                    <ButtonImage src="./icones/favorito.png" alt="Icone favorito"/>
+                    <ButtonImage src="./icones/expandir.png" alt="Icone expandir"/>
+                </Rodape>
             </figcaption>
-        </figure>
+        </Figure>
     )
 }
 
