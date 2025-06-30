d8b709c63c46c59015370c1eff6f1586e127581c
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-06-30 14:28:10 -0300

    finalizou modal

--- src/componentes/ModalZoom/index.tsx
+++ src/componentes/ModalZoom/index.tsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import Imagem from "../Galeria/Imagem";
 
 interface Foto {
@@ -13,18 +14,35 @@ interface ImagemProps {
     clickFotoSelecionada?: (foto: Foto) => void;
 } 
 
+const Overlay = styled.div`
+    background-color: rgba(0,0,0,0.7);
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+`
+
+const DialogEstilizado = styled.dialog`
+    position: absolute;
+    top: 294px;
+`
+
 const ModalZoom = ({foto, clickFotoSelecionada}:ImagemProps) => {
     //{!!foto} = true > false > true
     //js transforma obj foto em true se ela existir!!
     //se eu nao tenho um obj retornando em foto ele nega duas vezes e retorna false
     return(
         <>
-           {foto && <dialog open={!!foto}>
-                <Imagem foto={foto} expandida={true} clickZoom = {clickFotoSelecionada}/>
-                <form method="dialog">
-                    <button>OK</button>
-                </form>
-            </dialog>} 
+           {foto && <>
+            <Overlay/>
+                <DialogEstilizado open={!!foto}>
+                    <Imagem foto={foto} expandida={true} clickZoom = {clickFotoSelecionada}/>
+                    <form method="dialog">
+                        <button>OK</button>
+                    </form>
+                </DialogEstilizado>
+           </>} 
         </>
     )
 }
